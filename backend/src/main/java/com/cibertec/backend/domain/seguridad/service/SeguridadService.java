0c8bd8ee3b9cafb987270a9cfc6c72d8dcd6c79c
Miguel Soayne Magaño <legacykain777779@hotmail.com>
2025-08-29 09:15:53 -0500

    feat: agrega endpoint de perfil de usuario

--- backend/src/main/java/com/cibertec/backend/domain/seguridad/service/SeguridadService.java
+++ backend/src/main/java/com/cibertec/backend/domain/seguridad/service/SeguridadService.java
@@ -1,10 +1,13 @@
 package com.cibertec.backend.domain.seguridad.service;
 
 
+import com.cibertec.backend.application.seguridad.dto.UsuarioDto;
 import com.cibertec.backend.domain.seguridad.model.SeguridadModel;
 
 public interface SeguridadService {
     SeguridadModel autenticacion(String username, String password);
 
     SeguridadModel refrescar(String token);
+
+    UsuarioDto obtenerDetallesDeUsuario(String username);
 }
